0877d3db7843d4651c9fc5f1c58e952c8201ea3d
ASOST\2416017 <s.daiki122145@icloud.com>
2026-04-17 14:26:14 +0900

    a

---
+++ QUACK/Src/Application.cpp
@@ -0,0 +1,77 @@
+#include <DxLib.h>
+#include "Application.h"
+
+Application* Application::instance_ = nullptr;
+
+const std::string Application::PATH_MODEL = "Data/Model/";
+
+void Application::CreateInstance(void)
+{
+	if (instance_ == nullptr)
+	{
+		instance_ = new Application();
+	}
+	instance_->Init();
+}
+
+Application& Application::GetInstance(void)
+{
+	return *instance_;
+}
+
+void Application::Init(void)
+{
+
+	// ウィンドウサイズ
+	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 32);
+	ChangeWindowMode(true);
+	// DxLibの初期化
+	SetUseDirect3DVersion(DX_DIRECT3D_11);
+	isInitFail_ = false;
+	if (DxLib_Init() == -1)
+	{
+		isInitFail_ = true;
+		return;
+	}
+
+	// 乱数のシード値を設定する
+	DATEDATA date;
+
+	// 現在時刻を取得する
+	GetDateTime(&date);
+
+	// 乱数の初期値を設定する
+	// 設定する数値によって、ランダムの出方が変わる
+	SRand(date.Year + date.Mon + date.Day + date.Hour + date.Min + date.Sec);
+
+	// 入力制御初期化
+	SetUseDirectInputFlag(true);
+
+}
+
+void Application::Run(void)
+{
+
+}
+
+void Application::Destroy(void)
+{
+
+
+}
+
+bool Application::IsInitFail(void) const
+{
+	return isInitFail_;
+}
+
+bool Application::IsReleaseFail(void) const
+{
+	return isReleaseFail_;
+}
+
+Application::Application(void)
+{
+	isInitFail_ = false;
+	isReleaseFail_ = false;
+}
